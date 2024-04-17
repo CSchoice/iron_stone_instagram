22aea159fc953d5fe0d2c6b8ea22f10a1afde7ae
CSchoice <tkdlqjf@example.com>
2024-04-18 00:29:13 +0900

    Fix: 백과 프론트 cors 문제 해결을 위한 django-cors-headers 설치 및 settings.py 수정

--- contents/models.py
+++ contents/models.py
@@ -31,3 +31,4 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    
